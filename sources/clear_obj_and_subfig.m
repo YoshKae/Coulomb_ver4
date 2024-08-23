7d07e6d95ae837ea6960d7cc65c6ff9c9956ae46
YoshKae <yoshizawa.kaede.q1@dc.tohoku.ac.jp>
2024-08-23 22:18:19 +0900

    8/23 22:18

--- sources/clear_obj_and_subfig.m
+++ sources/clear_obj_and_subfig.m
@@ -1,14 +1,17 @@
-% script clear_obj_and_subfig
+% clear_obj_and_subfig
+% このスクリプトは、メインウィンドウのオブジェクトやサブフィギュアをクリアするためのものです。
 global H_MAIN
 
-subfig_clear;
-%--- clear all object on the main window
-ch = get(H_MAIN,'Children');
-n = length(ch) - 3;
+subfig_clear; % サブフィギュア（サブウィンドウや補助的なウィンドウ）をクリアするカスタム関数を呼び出します。
+
+%--- メインウィンドウ上のすべてのオブジェクトをクリアする
+ch = get(H_MAIN,'Children'); % メインウィンドウ内のすべての子オブジェクトを取得
+n = length(ch) - 3;          % 子オブジェクトの数を取得（最後の3つは保持）
 if n >= 1
     for k = 1:n
-        delete(ch(k));
+        delete(ch(k));       % 各子オブジェクトを削除
     end
+    % メインウィンドウのメニューバーとツールバーを非表示に設定
     set(H_MAIN,'Menubar','none','Toolbar','none');
 end
 %--- (end)
