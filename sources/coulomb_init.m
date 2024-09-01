353e03c0e3c0b6f0c493707816342114491712fc
YoshKae <yoshizawa.kaede.q1@dc.tohoku.ac.jp>
2024-09-01 21:05:20 +0900

    9/1 21:05

--- sources/coulomb_init.m
+++ sources/coulomb_init.m
@@ -1,6 +1,6 @@
 function coulomb_init
-%   This function initializes some of the global variable when Coulomb is
-%   launched. It also put some initial default values to several variables.
+% この関数は、Coulombプログラムが起動された際に、一部のグローバル変数を初期化し、
+% いくつかの変数に初期値を設定します。
 
 global HEAD NUM POIS CALC_DEPTH YOUNG FRIC R_STRESS ID KODE ELEMENT
 global FCOMMENT GRID SIZE SECTION
@@ -19,61 +19,64 @@ global OVERLAY_MARGIN
 global IND_RAKE
 global IMAXSHEAR
 
-% to detect a singular point (if IIRET > 0, singular)
+% 特異点を検出するためのフラグ (IIRET > 0の場合、特異点がある)
 IIRET = 0;
-% to clarify input file format
+% 入力ファイルフォーマットを明示するためのフラグ
 IRAKE = 0;
 
-% initialization for input parameters from an input file
-HEAD       = [];
-NUM        = [];
-POIS       = [];
-CALC_DEPTH = [];
-YOUNG      = [];
-FRIC       = [];
-R_STRESS   = [];
-ID         = [];
-KODE       = [];
-ELEMENT    = [];
-IND_RAKE   = [];
-FCOMMENT   = struct('ref',[]);
-GRID       = [];
-SIZE       = [];
-SECTION    = [];
-S_ELEMENT  = [];
-% flag for horizontal displ calc (0: no fixed point, 1: a fixed)
-FIXFLAG    = repmat(uint8(0),1,1);
-% default fixed point
+% 入力ファイルからのパラメータ初期化
+HEAD       = [];  % ヘッダー情報
+NUM        = [];  % 数値データ
+POIS       = [];  % ポアソン比
+CALC_DEPTH = [];  % 計算深度
+YOUNG      = [];  % ヤング率
+FRIC       = [];  % 摩擦係数
+R_STRESS   = [];  % 残留応力
+ID         = [];  % ID情報
+KODE       = [];  % コード情報
+ELEMENT    = [];  % 要素データ
+IND_RAKE   = [];  % 考慮されるすべりの方向
+FCOMMENT   = struct('ref',[]);  % コメントを格納する構造体
+GRID       = [];  % グリッド情報
+SIZE       = [];  % サイズ情報
+SECTION    = [];  % セクション情報
+S_ELEMENT  = [];  % S要素データ
+
+% 水平方向の変位計算のためのフラグ (0: 固定点なし, 1: 固定点あり)
+FIXFLAG    = repmat(uint8(0),1,1);  % 初期化：固定点なし
+% デフォルトの固定点を設定
 FIXX       = 0;
 FIXY       = 0;
 
-STRESS_TYPE = repmat(uint8(5),1,1);
-SHADE_TYPE  = repmat(uint8(1),1,1);
-SEC_FLAG    = repmat(uint8(0),1,1);
-LON_GRID    = [];
-LAT_GRID    = [];
+% 応力タイプとシェーディングタイプの初期化
+STRESS_TYPE = repmat(uint8(5),1,1);  % 応力タイプ
+SHADE_TYPE  = repmat(uint8(1),1,1);  % シェーディングタイプ
+SEC_FLAG    = repmat(uint8(0),1,1);  % セクションフラグ
+LON_GRID    = [];  % 経度グリッド
+LAT_GRID    = [];  % 緯度グリッド
 
-% flag to speed up EC calc to skip the max. shear calc routine
-IMAXSHEAR = 2; % 1: search, 2: skip
+% 最大剪断応力の計算をスキップするためのフラグ
+IMAXSHEAR = 2; % 1: 検索, 2: スキップ
 
-% initialization for overlay functions
-COAST_DATA  = [];
-AFAULT_DATA = [];
-EQ_DATA     = [];
-GPS_DATA    = [];
-GPS_FLAG      = 'horizontal';
-GPS_SEQN_FLAG = 'off';
+% オーバーレイ機能の初期化
+COAST_DATA  = [];  % 海岸線データ
+AFAULT_DATA = [];  % 活断層データ
+EQ_DATA     = [];  % 地震データ
+GPS_DATA    = [];  % GPSデータ
+GPS_FLAG      = 'horizontal';  % GPSフラグ（初期値: 水平）
+GPS_SEQN_FLAG = 'off';  % GPSシーケンスフラグ（初期値: オフ）
 
-% 3D viewpoint specification (azimuth and vertical elevation, degree)
-VIEW_AZ = 15;
-VIEW_EL = 40;
+% 3Dビューの指定 (方位角と垂直の角度、度単位)
+VIEW_AZ = 15;  % 方位角（初期値）
+VIEW_EL = 40;  % 垂直角度（初期値）
 
-% overlay function check item off
+% オーバーレイ機能のチェックアイテムをオフにする
 set(findobj('Tag','menu_gridlines'),'Checked','off');
 set(findobj('Tag','menu_coastlines'),'Checked','off');
 set(findobj('Tag','menu_activefaults'),'Checked','off');
 set(findobj('Tag','menu_earthquakes'),'Checked','off');
 
+% 深度範囲タイプと軸表示フラグの初期化
 DEPTH_RANGE_TYPE = repmat(uint8(0),1,1);
 FLAG_PR_AXES = repmat(uint8(0),1,1);
-OVERLAY_MARGIN = 5;
+OVERLAY_MARGIN = 5;  % オーバーレイのマージン（初期値）
