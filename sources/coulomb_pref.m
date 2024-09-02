9fdafae612729efb489f6617d50b29b89c5df685
YoshKae <yoshizawa.kaede.q1@dc.tohoku.ac.jp>
2024-09-02 14:53:33 +0900

    9/2 14:53

--- sources/coulomb_pref.m
+++ sources/coulomb_pref.m
@@ -1,7 +1,10 @@
-% This is a script for reading user's preference
-%
-global COUL_COLOR
-global mycmap
+% ユーザーの設定を読み込むためのスクリプト
 
-load('MyColormaps','mycmap');
-COUL_COLOR = 'mycmap';
+global COUL_COLOR  % カラーマップを格納する変数
+global mycmap  % カスタムカラーマップを格納する変数
+
+% 'MyColormaps' というファイルからカスタムカラーマップを読み込む
+load('MyColormaps', 'mycmap');  % 'mycmap' 変数にデータを読み込む
+
+% COUL_COLOR に 'mycmap' を設定
+COUL_COLOR = 'mycmap';  % カラーマップの設定をCOUL_COLORに適
--- sources/coulomb_registration_check.m
+++ sources/coulomb_registration_check.m
@@ -1,52 +1,55 @@
 function [flag] = coulomb_registration_check
-user_act1  = sum(license);
-serial_sum = 419;
-user_act   = user_act1 + serial_sum;
-abi = 0;
-homedir = pwd;
+% ソフトウェアのライセンスチェックを行う関数
+% 出力: flag = 'in' (ライセンス有効), 'out' (ライセンス無効)
 
+user_act1  = sum(license);           % MATLABのライセンス情報を取得し、その合計値を計算
+serial_sum = 419;                    % 予め定義されたシリアル番号の合計値
+user_act   = user_act1 + serial_sum; % シリアル番号とライセンス情報の合計値を計算
+abi = 0;                             % 初期化
+homedir = pwd;                       % 現在の作業ディレクトリを保存
 
-flag = 'in'; % default
-
+flag = 'in'; % デフォルトではライセンスが有効と設定
 
 try
-cd license    
-if exist('my_personal_license.txt') 
-   abi = textread('my_personal_license.txt');
-else
-   answer = inputdlg('Enter 8 digit serial number.','Serial Number');
-   user_serial = [answer{:}];
-   sum(user_serial);
-   if sum(user_serial)==serial_sum
-        abi = sum(user_serial) + user_act1;
-        save my_personal_license.txt abi -ascii
-   else
-       flag = 'out';
-       cd homedir
-       return
-   end
+    cd license % 'license'ディレクトリに移動
+    if exist('my_personal_license.txt') % ライセンスファイルが存在するか確認
+        abi = textread('my_personal_license.txt'); % ファイルからライセンス情報を読み込む
+    else
+        answer = inputdlg('Enter 8 digit serial number.','Serial Number'); % シリアル番号を入力するダイアログを表示
+        user_serial = [answer{:}]; % 入力されたシリアル番号を取得
+        sum(user_serial);
+        if sum(user_serial)==serial_sum % 入力されたシリアル番号が正しいか確認
+            abi = sum(user_serial) + user_act1; % シリアル番号とライセンス情報の合計値を計算
+            save my_personal_license.txt abi -ascii % 計算されたライセンス情報をファイルに保存
+        else
+        flag = 'out'; % シリアル番号が間違っている場合、ライセンス無効として設定
+        cd homedir % 元のディレクトリに戻る
+        return
+    end
 end
-cd ..
+cd .. % 元のディレクトリに戻る
 
 if abi ~= user_act
     cd license
-        answer = inputdlg('Enter 8 digit serial number.','Serial Number');
+        answer = inputdlg('Enter 8 digit serial number.','Serial Number'); % 再度シリアル番号を入力するダイアログを表示
         user_serial = [answer{:}];
         sum(user_serial);
-        abi = sum(user_serial) + user_act1;
-        save my_personal_license.txt abi -ascii
+        abi = sum(user_serial) + user_act1; % 新たに計算されたライセンス情報を更新
+        save my_personal_license.txt abi -ascii % 新しいライセンス情報をファイルに保存
     cd ..
-    if abi ~= user_act
-    disp(' ');
-    disp('A proler license file is not found. To use Coulomb, you need a serial number first.');
-    disp('Send email to Volkan Sevilgen <vsevilgen@usgs.gov> to get the number');
-    flag = 'out';
-    return
+    if abi ~= user_act % それでも一致しない場合
+        disp(' ');
+        disp('A proler license file is not found. To use Coulomb, you need a serial number first.');
+        disp('Send email to Volkan Sevilgen <vsevilgen@usgs.gov> to get the number');
+        disp('有効なライセンスファイルが見つかりません。Coulombを使用するには、シリアル番号が必要です。');
+        disp('Volkan Sevilgen <vsevilgen@usgs.gov> に連絡してシリアル番号を取得してください。');
+        flag = 'out'; % ライセンス無効として設定
+        return
     end
 else
-    flag = 'in';
+    flag = 'in'; % ライセンスが有効な場合
 end
 
 catch
-    cd homedir
+    cd homedir % 例外が発生した場合、元のディレクトリに戻る
 end
