85f0210fa1898493322f5834c992a0a67d5b6a26
Kaede Yoshizawa <yoshizawa.kaede.q1@dc.tohoku.ac.jp>
2024-09-03 03:57:18 +0900

    9/3 3:57

--- sources/disp_sect_conv.m
+++ sources/disp_sect_conv.m
@@ -1,8 +1,12 @@
 function [uu] = disp_sect_conv(x0,y0,x1,y1,uux,uuy)
-% this function converts a given horizontal unit displacement (uux, uuy)
-% to a given cross section plane as uu
-%  **** caution **** all parameters should be scalar.
+% disp_sect_conv 関数は、与えられた水平単位変位 (uux, uuy) を
+% 特定の断面平面上に投影し、その結果を uu として返します。
+% **** 注意 **** すべてのパラメータはスカラーである必要があります。
 
+% 入力パラメータ:
+%   x0, y0 : 始点の座標
+%   x1, y1 : 終点の座標
+%   uux, uuy : 水平単位変位
 % x0 = 0;
 % y0 = 0;
 % x1 = 40;
@@ -10,20 +14,24 @@ function [uu] = disp_sect_conv(x0,y0,x1,y1,uux,uuy)
 % uux = -0.7;
 % uuy = 1.0;
 
-% in case
+% y1がy0と同じ場合に対処（ゼロ除算を避けるためにy1をわずかに調整）
 if y1 == y0
     y1 = y1 + 0.0001;
 end
 
-strk   = rad2deg(atan((x1-x0)/(y1-y0)));
-u_strk = rad2deg(atan(uux/uuy));
-uxy = sqrt(uux*uux + uuy*uuy);
+% 断面のストライク角を計算
+strk = rad2deg(atan((x1 - x0) / (y1 - y0)));
+% 水平変位のストライク角を計算
+u_strk = rad2deg(atan(uux / uuy));
+% 水平変位の大きさを計算
+uxy = sqrt(uux * uux + uuy * uuy);
 
+% 始点と終点の位置関係に応じて角度を計算
 if x1 > x0
-    if y1 > y0          % -------- X+ & Y+
+    if y1 > y0          % X+ & Y+ の場合
         if uux > 0
             if uuy > 0
-                ang = abs(u_strk - strk);
+                ang = abs(u_strk - strk); % 変位角と断面角の差の絶対値を計算
             else
                 ang = abs(u_strk+180. - strk);
             end
@@ -34,8 +42,7 @@ if x1 > x0
                 ang = abs(u_strk - strk);
             end
         end        
-    else                % -------- X+ & Y-
-        
+    else                % X+ & Y- の場合
         if uux < 0
             if uuy > 0
                 ang = abs(u_strk - strk);
@@ -49,11 +56,9 @@ if x1 > x0
                 ang = abs(u_strk - strk);
             end
         end  
-        
-        
     end
 else
-    if y1 > y0          % -------- X- & Y+
+    if y1 > y0          % X- & Y+ の場合
 
        if uux > 0
             if uuy > 0
@@ -67,11 +72,8 @@ else
             else
                 ang = abs(u_strk - strk);
             end
-        end  
-        
-        
-    else                % -------- X- & Y-
-
+        end     
+    else                % X- & Y- の場合
         if uux > 0
             if uuy > 0
                 ang = abs(u_strk+180. - strk);
@@ -89,9 +91,10 @@ else
     end
 end
 
-% ang = u_strk - strk
-
+% 断面平面上に投影された変位の大きさを計算
 uu = cos(deg2rad(ang)) * uxy;
+
+% 水平単位変位のx成分が負の場合、符号を反転
 if uux < 0.0
     uu = -uu;
 end
